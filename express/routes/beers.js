3cc68c12a32197872ffe00f9760e4be1ceafe36c
Anderson Cardozo <kledac@gmail.com>
2014-08-27 22:59:36 -0300

    day 3 final

--- express/routes/beers.js
+++ express/routes/beers.js
@@ -37,6 +37,19 @@ var cbEdit = function(req,res,err,data,mensagem){
     res.render('beers/edit', { title: 'Adega Be MEAN', msg: 'Alteração de cerveja', beer:data });
 }
 
+var cbDelete = function(req,res,err,data,mensagem){
+    
+    if (err){
+        console.log('Erro: ', err);
+        msg =   'Erro: '  + err;
+      }else{
+        console.log(mensagem, data);    
+        msg = data;
+      }
+    
+    res.render('beers/delete', { title: 'Adega Be MEAN', msg: 'Excluir cerveja', beer:data });
+}
+
 
 router.get('/create', function(req, res) {
   res.render('beers/create', { title: 'Adega Be MEAN', msg: 'Criar cerveja' });
@@ -46,6 +59,12 @@ router.get('/create', function(req, res) {
 router.get('/:id/edit', function(req, res) {
   Controller.getBeer(req,res, cbEdit)
 });
+
+
+router.get('/:id/delete', function(req, res) {
+  Controller.getBeer(req,res, cbDelete)
+});
+
 
 
 
